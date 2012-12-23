95d9511965101cf6f71c47a5a078d38b9baeba77
Kalpazan <kalpazang@gmail.com>
2012-12-23 20:59:19 +0200

    micro fix

--- src/com/example/MetroStations.java
+++ src/com/example/MetroStations.java
@@ -6,24 +6,26 @@ public class MetroStations {
 
     private static HashMap<String, Integer> metroStations;
 
-    public static synchronized HashMap getStations() {
-        if (metroStations == null) {
+    static {
 
-            metroStations = new HashMap<String, Integer>();
-            metroStations.put("Academgorodok", 29931);
-            metroStations.put("Zhitomirskaya", 29930);
-            metroStations.put("Svyatoshin", 29929);
-            metroStations.put("Nyvky", 29928);
-            metroStations.put("Beresteyskaya", 29927);
-            metroStations.put("Shulyavskaya", 29926);
-            metroStations.put("Polytechnicheskiy Universitet", 29925);
-            metroStations.put("Vokzalnaya", 29924);
-            metroStations.put("Universitet", 29923);
-            metroStations.put("Teatralnaya", 29922);
-            metroStations.put("Khreshatyk", 29921);
-            metroStations.put("Arsenalnaya", 29920);
-        }
-        return metroStations;
+        metroStations = new HashMap<String, Integer>();
+
+        metroStations.put("Academgorodok", 29931);
+        metroStations.put("Zhitomirskaya", 29930);
+        metroStations.put("Svyatoshin", 29929);
+        metroStations.put("Nyvky", 29928);
+        metroStations.put("Beresteyskaya", 29927);
+        metroStations.put("Shulyavskaya", 29926);
+        metroStations.put("Polytechnicheskiy Universitet", 29925);
+        metroStations.put("Vokzalnaya", 29924);
+        metroStations.put("Universitet", 29923);
+        metroStations.put("Teatralnaya", 29922);
+        metroStations.put("Khreshatyk", 29921);
+        metroStations.put("Arsenalnaya", 29920);
+
     }
 
+    public static HashMap<String, Integer> getMetroStations() {
+        return metroStations;
+    }
 }
